17f5e94bb8d4b8c9405a224f714514de6e22d133
ajengrhhh <147266996+ajengrhhh@users.noreply.github.com>
2025-04-20 17:04:00 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "https://tu6-be060-700231807331.us-central1.run.app/notes";
+const API_URL = "https://tu6-be060-700231807331.us-central1.run.app";
 
 function App() {
   const [notes, setNotes] = useState([]);
